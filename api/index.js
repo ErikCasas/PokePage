e5a9173af37b02850567f3b9ab4a8f438fbba4be
Erik Casas <santicasas667@gmail.com>
2023-01-09 10:03:01 -0500

    ESTE ES

--- api/index.js
+++ api/index.js
@@ -1,3 +1,14 @@
+const server = require('./src/app.js');
+const { conn } = require('./src/db.js');
+require('dotenv').config();
+const {PORT} = process.env; 
+// Syncing all the models at once.
+conn.sync({ force: false }).then(() => {
+  server.listen(process.env.PORT, () => {
+    console.log('%s listening at ', process.env.PORT); // eslint-disable-line no-console
+  });
+});
+    
 //                       _oo0oo_
 //                      o8888888o
 //                      88" . "88
@@ -17,13 +28,3 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
-
-// Syncing all the models at once.
-conn.sync({ force: false }).then(() => {
-  server.listen(3003, () => {
-    console.log('%s listening at 3003'); // eslint-disable-line no-console
-  });
-});
-    
